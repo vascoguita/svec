cc83920f6977d9d44a6697b94184847340899d6a
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2014-01-16 10:55:14 +0100

    software/sdb-flash: updated flash layout with increased bitstream size & use new bootloader version

--- software/sdb-flash/build.sh
+++ software/sdb-flash/build.sh
@@ -2,10 +2,20 @@
 
 # A trivial script to build the SDB flash image for the SVEC. Requires sdb-tools installed in the system
 
-#cp ../../hdl/syn/sfpga_bootloader/svec_sfpga_top.bin fs/bootldr.bin
-#cp ../../hdl/syn/golden/svec_top.bin fs/afpga.bin
-gcc bin2vmf.c -o bin2vmf
-gcc bin2ihex.c -o bin2ihex
+PROMGEN=`which promgen`
+GENSDBFS=`which gensdbfs`
+
+if [ ! -f "$PROMGEN" ]; then
+	echo "You seem to not have the promgen utility. Do you have Xilinx ISE installed?"
+	exit
+fi
+
+if [ ! -f "$GENSDBFS" ]; then
+	echo "You seem to not have the gensdbfs. Have you compiled and installed it (check the manual)?"
+	exit
+fi
+
+
 gensdbfs fs image.bin
+promgen -p mcs -o image.mcs -spi -data_file up 0 image.bin  -w
 ./bin2vmf image.bin > image.vmf
-./bin2ihex -e -o image.mcs image.bin
