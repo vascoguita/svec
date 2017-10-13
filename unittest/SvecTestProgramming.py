887a35e866a354ae97da864514b2dba0269d49d4
Federico Vaga <federico.vaga@vaga.pv.it>
2017-10-13 16:39:55 +0200

    unittest: add a programming test
    It takes a bitstream from the environment and it writes it.
    
    export BITSTREAM=/path/to/bitstream.bin
    export VME_SLOT=8
    python -m unittest -v SvecTestProgramming.py
    
    Signed-off-by: Federico Vaga <federico.vaga@vaga.pv.it>

---
+++ unittest/SvecTestProgramming.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+
+
+class SvecTestProgramming(unittest.TestCase):
+    def setUp(self):
+        slot = os.environ["VME_SLOT"]
+        self.file_path = "/sys/bus/vme/devices/vme.{}/svec/svec.{}/AFPGA/lock".format(slot, slot)
+        self.dev_path = "/dev/svec.{}".format(slot)
+        self.bitstream = os.environ["BITSTREAM"]
+
+    def test_01(self):
+        """It writes a dummy FPGA bitstream"""
+        with open(self.file_path, "w") as f:
+            f.write("unlock")
+
+        with open(self.bitstream, "rb") as d:
+            with open(self.dev_path, "wb") as f:
+                f.write(d.read())
+
+        with open(self.file_path, "r") as f:
+            val = f.read().strip()
+        self.assertEqual(val, "locked")
