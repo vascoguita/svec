46bd7e78b785863fef24bb9dcb00f4bc937039c7
Tristan Gingold <tristan.gingold@cern.ch>
2019-09-23 13:45:10 +0200

    Rename template -> base

--- hdl/syn/common/Manifest.py
+++ hdl/syn/common/Manifest.py
@@ -1,18 +1,18 @@
-# User should define the variable svec_template_ucf
+# User should define the variable svec_base_ucf
 
-files = [ "svec_template_common.ucf" ]
+files = [ "svec_base_common.ucf" ]
 
 ucf_dict = {
-    'ddr4': "svec_template_ddr4.ucf",
-    'ddr5': "svec_template_ddr5.ucf",
-    'wr':   "svec_template_wr.ucf",
-    'led':  "svec_template_led.ucf",
-    'gpio': "svec_template_gpio.ucf",
+    'ddr4': "svec_base_ddr4.ucf",
+    'ddr5': "svec_base_ddr5.ucf",
+    'wr':   "svec_base_wr.ucf",
+    'led':  "svec_base_led.ucf",
+    'gpio': "svec_base_gpio.ucf",
 }
 
-for p in svec_template_ucf:
+for p in svec_base_ucf:
     f = ucf_dict.get(p, None)
-    assert f is not None, "unknown name {} in 'svec_template_ucf'".format(p)
+    assert f is not None, "unknown name {} in 'svec_base_ucf'".format(p)
     if p == 'ddr4' or p == 'ddr5':
-        files.append('svec_template_ddr_common.ucf')
+        files.append('svec_base_ddr_common.ucf')
     files.append(f)
