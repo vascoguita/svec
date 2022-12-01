3ed4b39cc179358fc2643b568889357f776e3ae7
Dimitris Lampridis <dimitris.lampridis@cern.ch>
2022-12-01 14:46:50 +0100

    hdl: delete sim folder
    These files, when needed are now available from dependencies in hdl/ip_cores.
    
    (for example, ddr memory model can be found in ddr controller project).
    
    There were also some obsolete testbenches in here.

--- hdl/sim/ddr_test/sim_models/2048Mb_ddr3/Manifest.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2022 CERN (home.cern)
-#
-# SPDX-License-Identifier: CERN-OHL-W-2.0
-
-files = ["ddr3.v"]
-
-vlog_opt = "+incdir+sim_models/2048Mb_ddr3 +define+sg15E +define+x16"
